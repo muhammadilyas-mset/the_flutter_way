d1b78e847a7ebc789b89f3177999009dfc15789f
Muhammad Ilyas <muhammad_ilyas192@hotmail.com>
2023-10-30 22:58:46 +0500

    add(assignment2): added calculate function and calculate the tip amount

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-
-/// Flutter code sample for [Slider].
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -17,13 +15,25 @@ class MyApp extends StatelessWidget {
 
 class SliderExample extends StatefulWidget {
   const SliderExample({super.key});
-
   @override
   State<SliderExample> createState() => _SliderExampleState();
 }
 
 class _SliderExampleState extends State<SliderExample> {
-  double _currentSliderValue = 10;
+  double billAmountTip = 0;
+  double tipPercentageTotal = 0;
+  double tipAmountSlide = 0;
+  var totalAmounttipTextFieldState = "0";
+
+  void calculate() {
+    if (totalAmounttipTextFieldState.isNotEmpty) {
+      var amount = double.parse(totalAmounttipTextFieldState);
+      if (amount > 0) {
+        billAmountTip = (amount * tipAmountSlide) / 100;
+        tipPercentageTotal = amount + billAmountTip;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +43,25 @@ class _SliderExampleState extends State<SliderExample> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(),
-          const SizedBox(
+          SizedBox(
             height: 150,
-            child: MyCustomTextField(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+              child: TextField(
+                onChanged: (value) => setState(() {
+                  totalAmounttipTextFieldState = value;
+                  calculate();
+                }),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter the bill amount...',
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
           ),
           const SizedBox(
             child: Text(
@@ -44,50 +70,25 @@ class _SliderExampleState extends State<SliderExample> {
             ),
           ),
           Container(
-            padding: const EdgeInsets.all(0),
+            padding: const EdgeInsets.all(10),
             child: Slider(
-              value: _currentSliderValue,
+              value: tipAmountSlide,
               max: 100,
               activeColor: const Color.fromARGB(255, 76, 155, 175),
               inactiveColor: const Color.fromARGB(255, 0, 183, 255),
-              divisions: 10,
-              label: _currentSliderValue.round().toString(),
-              onChanged: (double value) {
-                setState(() {
-                  _currentSliderValue = value;
-                });
-              },
+              divisions: 100,
+              label: tipAmountSlide.round().toString(),
+              onChanged: (value) => setState(() {
+                tipAmountSlide = value;
+                calculate();
+              }),
             ),
           ),
-          Container(
-            height: 100,
-            color: const Color.fromARGB(255, 59, 160, 255),
-          ),
+          Text("Tip PKR: $billAmountTip"),
+          const SizedBox(height: 20),
+          Text("Total PKR: $tipPercentageTotal"),
         ],
       ),
-    );
-  }
-}
-
-class MyCustomTextField extends StatelessWidget {
-  const MyCustomTextField({super.key});
-
-  @override
-  // ignore: prefer_const_constructors
-  Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter the bill amount...',
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
